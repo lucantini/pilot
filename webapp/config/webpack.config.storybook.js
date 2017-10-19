4c2ec9ae597c65accaeb1dcf998feb4e969826d0
Derek Stavis <dekestavis@gmail.com>
2017-10-19 17:57:04 -0200

    date-selector: add initial implementation
    Co-authored: @felquis, @derekstavis

--- webapp/config/webpack.config.storybook.js
+++ webapp/config/webpack.config.storybook.js
@@ -84,6 +84,14 @@ module.exports = {
         ],
       },
       {
+        test: /\.scss/,
+        use: [
+          { loader: require.resolve('style-loader') },
+          { loader: require.resolve('css-loader') },
+          { loader: require.resolve('sass-loader') },
+        ]
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|svg)$/,
         loader: require.resolve('file-loader'),
         options: {
